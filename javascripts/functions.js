63b6949f0f3a307c6030cae56d28b67843c329a2
Griffin Banta <gbanta@uoregon.edu>
2020-04-02 16:26:10 -0700

    Add arrMax to functions.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,9 +1,23 @@
 // jshint esversion: 7
 
+//3B
 let arrAvg = inputArray => {
   let sum = 0;
   for (var i = 0; i < inputArray.length; i++) {
     sum += inputArray[i];
   }
   return sum / inputArray.length;
+};
+
+//3C
+let arrMax = inputArray => {
+  let max = inputArray[0];
+
+  let maxFunction = value => {
+    if (value > max) {
+      max = value;
+    }
+  };
+  inputArray.forEach(maxFunction);
+  return max;
 };
